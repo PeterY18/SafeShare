bffb7cf359046b8e87d55fb5a98e65079f629f5b
Peter Yuan <peter.yuan18@gmail.com>
2022-06-08 19:10:54 -0400

    Add db insertion

--- routes/account.js
+++ routes/account.js
@@ -1,6 +1,21 @@
 const express = require("express")
 const router = express.Router()
 
+const crypto = require("crypto")
+const algorithm = "aes-256-cbc"
+const initVector = crypto.randomBytes(16)
+const securityKey = crypto.randomBytes(32)
+const cipher = crypto-crypto.createCipheriv(algorithm, securityKey, initVector)
+
+require('dotenv').config();
+
+const {MongoClient, ObjectId} = require('mongodb');
+
+const url = "mongodb+srv://SafeShare:sAlWpKNC6jkncmgT@cluster0.apg9o.mongodb.net/?retryWrites=true&w=majority"
+const client = new MongoClient(url);
+
+const dbName = "account";
+
 router.get("/", (req, res) => {
     res.sendStatus(404)
 })
@@ -9,7 +24,48 @@ router.get("/upload", (req, res) => {
     res.render("accountForm")
 })
 
+async function main() {
+
+    try {
+        await client.connect();
+        console.log("Connected correctly to server");
+        const db = client.db(dbName);
+        // Use the collection "people"
+       } catch (err) {
+        console.log(err.stack);
+    }
+}
+
 router.post("/upload/done", (req, res) => {
+    // send password to database
+    
+    const db = client.db(dbName);
+    const col = db.collection("info");
+
+    let infoDoc = {
+        "password": req.body.password,
+        _id: createId()
+    }
+    // Insert a single document, wait for promise so we can read it back
+    console.log("Before DB insert")
+    console.log("password: " + infoDoc.password)
+    console.log("id: " + infoDoc._id)
+    console.log()
+    const p = col.insertOne(infoDoc);
+    // Find one document
+    const searchId = new ObjectId(infoDoc.id)
+    const myDoc = col.findOne(searchId);
+    // Print to the console
+    console.log("After DB insert")
+    console.log(myDoc);
+    myDoc.then((result) => {
+        console.log(result)
+    })
+
+
+    // console.log(col.findOne(1))
+
+
     res.render("accountFormDone", {password: req.body.password})
 })
 
@@ -20,5 +76,14 @@ router.get("/:id/download", (req, res) => {
     // return password with that id
     res.render("accountLink")
 })
+
+function createId() {
+    let result = ""
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    for (var i = 0; i < 32; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length))
+    }
+    return result;
+}
 
 module.exports = router
