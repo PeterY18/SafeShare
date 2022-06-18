de988b00f963859f941bc84dc9ddeed1429a1c49
unknown <mthoden@gmu.edu>
2022-06-17 20:01:00 -0400

    added file upload test

--- tests/db.test.js
+++ tests/db.test.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose');
+const multer = require("multer")
+const GridFsStorage = require("multer-gridfs-storage").GridFsStorage
+const Grid = require("gridfs-stream")
+const express = require("express")
+const router = express.Router()
 
 const {MongoClient} = require('mongodb');
 
@@ -7,6 +12,8 @@ const client = new MongoClient(url);
 const conn = mongoose.createConnection(url)
 const dbName = "test";
 let db
+let gfs
+let gridfsBucket
 
 const items = mongoose.model("test", mongoose.Schema({
     name: String
@@ -43,6 +50,52 @@ async function main() {
 
     expect(i).not.toBe(null)
     conn.dropCollection(col)
+ })
+
+ test("file uploads", () => {
+   const storage = new GridFsStorage({
+      url: url,
+      file: (req, file) => {
+          return new Promise((resolve, reject) => {
+              const fileInfo = {
+                  fileName: fileName,
+                  bucketName: 'testFiles'
+              };
+              resolve(fileInfo);
+         });
+      }
+   });
+
+   const upload = multer({ storage });
+
+   conn.once("open", () => {
+      gridfsBucket = new mongoose.mongo.GridFSBucket(conn.db, {
+         bucketName: 'testFiles'
+      })
+   })
+
+   router.post("/upload/done", upload.single("file"), (req, res) => {
+      const db = client.db(dbName);
+      const col = db.collection("testFiles");
+  
+      let fileDoc = {
+         fileName: "NAME"
+      }
+      
+      const p = col.insertOne(fileDoc);
+  })
+
+  router.get("/:id", (req, res) => {
+      const db = client.db(dbName)
+      const col = db.collection("tester")
+      const fileCol = db.collection("testFiles")
+
+      const myDoc = gfs.collection("testFiles").findOne()
+      const fileDoc = fileCol.findOne()
+
+      expect(myDoc).not.toBe(null)
+      gfs.dropCollection("testFiles")
+   })
  })
 
  client.close();
